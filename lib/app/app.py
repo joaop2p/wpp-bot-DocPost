0047e7f4b43ea3b4b9d855f294e5882012852a4b
João <joaopauloxavier9@gmail.com>
2025-11-05 16:37:29 -0300

    Refatora a configuração de ações para utilizar ActionsConfig e ajusta a passagem de parâmetros nas rotinas Despatch e CheckSentMessages.

--- lib/app/app.py
+++ lib/app/app.py
@@ -3,24 +3,26 @@ from lib.config import STATUS
 from lib.src.models.interfaces.routines import Routine
 from lib.src.routines.check_sent_messages import CheckSentMessages
 from ..src.routines.despatch import Despatch
-from mytools.web.advanced.actions import Actions
+from mytools.PyZapp.advanced.actions_v2 import ActionsConfig
+
+# from mytools.web.advanced.actions import Actions
 from ..config import CONFIG, DEFAULTS
 
 class App:
     def __init__(self) -> None:
         self.logger = logging.getLogger(self.__class__.__name__)
-        self.actions = Actions()
+    
 
-    def _config_actions(self) -> None:
-        self.actions.set_driver_config(CONFIG.headless_mode, CONFIG.path.cache_driver_path)
-        self.actions.set_path_config(CONFIG.path.repository_pdf)
+    def _config_actions(self) -> ActionsConfig:
+        config: ActionsConfig = ActionsConfig(headless=CONFIG.headless_mode, driver_path=CONFIG.path.cache_driver_path, cache_path=CONFIG.path.repository_pdf)
+        return config
 
     def run(self) -> STATUS | STATUS:
         self.logger.info(f'Aplicação iniciada, versão: {DEFAULTS.VERSION}')
-        self._config_actions()
+        config = self._config_actions()
         routines: list[Routine] = [
-            Despatch(self.actions),
-            CheckSentMessages(self.actions)
+            Despatch(config),
+            CheckSentMessages(config)
         ]
         code = STATUS.SUCCESS
         for routine in routines:
