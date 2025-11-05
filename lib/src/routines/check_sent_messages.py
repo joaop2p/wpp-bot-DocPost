5ca8f936a0f682f73a7eff0cb6f3e9453a26e94b
João <joaopauloxavier9@gmail.com>
2025-11-05 16:37:40 -0300

    Refatora a classe CheckSentMessages para utilizar ActionsConfig e ajusta a passagem de parâmetros na verificação de mensagens entregues.

--- lib/src/routines/check_sent_messages.py
+++ lib/src/routines/check_sent_messages.py
@@ -1,5 +1,6 @@
 import logging
-from mytools.web.advanced.actions import Actions
+# from mytools.web.advanced.actions import Actions
+from mytools.PyZapp.advanced.actions_v2 import Actions, ActionsConfig
 from lib.config import CONFIG
 from lib.src.controls.file_control import FileControl
 from lib.src.controls.package_control import PackageControl
@@ -7,25 +8,25 @@ from lib.src.models.interfaces.routines import Routine
 from mytools.structs.files import ProcessesList
 
 class CheckSentMessages(Routine):
-    def __init__(self, actions: Actions) -> None:
-        self.actions = actions
+    def __init__(self, config: ActionsConfig) -> None:
+        self.config = config
         self.file_control = FileControl()
         self._logger = logging.getLogger(self.__class__.__name__)
 
-    def _check_message_delivered(self, num_used: int, num_process: int, file_name: str) -> bool:
+    def _check_message_delivered(self, action: Actions, num_used: int, num_process: int, file_name: str) -> bool:
         self._logger.info(f'Verificando se a mensagem do processo {num_process} foi entregue...')
-        if not self.actions.wpp_started:
-            self.actions.start_whatsapp()
-        self.actions.search(num_used)
-        result = self.actions.delivered()
+        if not action.wpp_started:
+            action.start_whatsapp()
+        action.search(num_used)
+        result = action.delivered()
         if result:
-            self.actions.safe_search(num_process, False)
-            self.actions.print_page(num_process)
+            action.safe_search(num_process, False)
+            action.print_page(num_process)
             self.file_control.move_to_ready(path=file_name)
             self._logger.info('Mensagem entregue com sucesso.')
         else:
             self._logger.info('Mensagem não entregue.')
-        self.actions.exit_chat_from_search()
+        action.close_chat()
         return result
 
     def run(self) -> None:
@@ -35,10 +36,11 @@ class CheckSentMessages(Routine):
         if not pending:
             self._logger.info('Nenhum pacote pendente encontrado.')
             return
-        for item in pending:
-            process = process_list.find_process(item.process_id)
-            if process and process.files:
-                if self._check_message_delivered(item.num_used, item.process_id, process.files[0].directory):
-                    item.delivered = True
-                    package_control.insert_package(item)
+        with Actions(self.config) as actions:
+            for item in pending:
+                process = process_list.find_process(item.process_id)
+                if process and process.files:
+                    if self._check_message_delivered(actions, item.num_used, item.process_id, process.files[0].directory):
+                        item.delivered = True
+                        package_control.insert_package(item)
         self._logger.info('Rotina finalizada.')
